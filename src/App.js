b823867009f3faa329c171b5ac4dafe8c8b1b403
anna dodo <anna.dorenskaya@gmail.com>
2020-10-15 21:22:07 +0200

    smth

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+/* import logo from './react-logo.svg';
+import icon1 from '../public/images/icon1.png'
+import icon2 from '../public/images/icon2.png'
+import icon3 from '../public/images/icon3.png'
+import icon4 from '../public/images/icon4.png' */
+
+ 
+function App() {
+  return <div className="App">smth</div>;
+   <img src={icon1} />;
+   <img src={icon2} />;
+   <img src={icon3} />;
+   <img src={icon4} />; 
+}
+
+//let element = React.createElement = 
+ 
+export default App;
